b7ddbc6b07bb82bea3ca35f77bd119896ac751b2
escwademographics <138754610+escwademographics@users.noreply.github.com>
2024-02-06 12:28:07 +0200

    Update Djibouti.py

--- Djibouti.py
+++ Djibouti.py
@@ -66,7 +66,7 @@ if submit and email == actual_email and password == actual_password:
     with col7:
         st.write("[Housing Conditions - المساكن](https://docs.google.com/spreadsheets/d/10WXo1MyEdiWQwee9d3XsDlJzBJozGsUa/edit?usp=drive_link&ouid=111028980933962818091&rtpof=true&sd=true)")   
 
-    st.title("Questionnaires in English Language are availble below:")
+    st.title("Questionnaires in English Language are available below:")
 
     
     coll1, coll2, coll3, coll4, coll5, coll6, coll7 = st.columns(7)
